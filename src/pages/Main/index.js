bb033322039bf591be7f554f4250c2edc1fc6b3a
DanPHP7 <danphp7@gmail.com>
2019-11-21 20:50:40 -0300

    parametros nas rotas

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import api from '../services/api';
 import { Container, Form, SubmitButton, List } from './styles';
 
@@ -73,10 +74,12 @@ class Main extends Component {
           </SubmitButton>
         </Form>
         <List>
-          {repositories.map((repository, index) => (
-            <li key={index}>
+          {repositories.map(repository => (
+            <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="#">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
